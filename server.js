0fdc3ad741c871557ee16fd2c682f4aac9ceab59
lilvina <davinaleo2007@yahoo.com>
2019-03-22 09:27:29 -0700

    finished routes for action

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const helmet = require('helmet')
 
 const server = express()
+const actionRouter = require('./actions/actionRouter.js')
+//const projectRouter = require('./projects/projectRouter.js')
 
 server.use(express.json())
 server.use(helmet())
+
+server.use('/api/action', actionRouter)
 
 server.get('/', (req, res) => {
   res.send(`
